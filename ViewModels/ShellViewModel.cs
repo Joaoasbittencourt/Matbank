4854fea33022d3c22dcddfbc95a5552f6bea4e94
João Bittencourt <joao@startapp.one>
2019-02-18 01:31:34 -0300

    Main Form is now  a conductor

--- ViewModels/ShellViewModel.cs
+++ ViewModels/ShellViewModel.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 
 namespace Matbank.ViewModels
 {
-    public class ShellViewModel: Screen
+    public class ShellViewModel: Conductor<IScreen>
     {
         public ShellViewModel()
         {

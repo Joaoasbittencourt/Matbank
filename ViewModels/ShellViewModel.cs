d01dd3ae27c686573cd29a768ec00b315769b3f1
João Bittencourt <joao@startapp.one>
2019-02-17 23:17:58 -0300

    added item model and list

--- ViewModels/ShellViewModel.cs
+++ ViewModels/ShellViewModel.cs
@@ -1,9 +1,46 @@
 ﻿using Caliburn.Micro;
+using Matbank.Models;
+using System.Collections.Generic;
 
 namespace Matbank.ViewModels
 {
     public class ShellViewModel: Screen
     {
+
+        public ShellViewModel()
+        {
+            Items.Add(new ItemModel {  });
+        }
+
+        private string _itemName;
+        public string ItemName
+        {
+            get { return _itemName; }
+            set
+            {
+                _itemName = value;
+                NotifyOfPropertyChange(() => ItemName);
+            }
+        }
+
+        private BindableCollection<ItemModel> _items = new BindableCollection<ItemModel>();
+        public BindableCollection<ItemModel> Items
+        {
+            get { return _items; }
+            set { _items = value; }
+        }
+
+        private ItemModel _selectedItem;
+
+        public ItemModel SelectedItem
+        {
+            get { return _selectedItem; }
+            set
+            {
+                _selectedItem = value;
+                NotifyOfPropertyChange(() => SelectedItem);
+            }
+        }
 
     }
 }
